214e85233b09764fecb2ab5844b334c677cc4007
ruigodoy <rui.godoy@hotmail.com>
2020-11-22 17:39:56 -0300

    fixing bugs

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const ContatoController = require('./controllers/contatoController')
 const agendaController = require('./controllers/agendaController')
-const profileController = require('./controllers/profileController')
 const sessionController = require('./controllers/sessionController');
 const contatoController = require('./controllers/contatoController');
 
@@ -13,8 +12,6 @@ routes.post('/sessions', sessionController.create)
 
 routes.get('/agenda', agendaController.index);
 routes.post('/agenda', agendaController.create);
-
-routes.get('/profile', profileController.index)
 
 routes.get('/contato/:id', contatoController.getOne)
 routes.get('/contato', ContatoController.index);
